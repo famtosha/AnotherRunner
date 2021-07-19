9097987e8e95eba4493070edf0a03602584b4e38
Famtosha <artem_kazakovtsev@mail.ru>
2021-07-19 18:14:36 +0500

    Added: saw

---
+++ Assets/Scripts/LevelObjects/DeadZone.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class DeadZone : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.TryGetComponent<Player>(out var player))
+        {
+            player.Kill();
+        }
+    }
+}
---
+++ Assets/Scripts/LevelObjects/Saw.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class Saw : MonoBehaviour
+{
+    [SerializeField] private Transform _sawStart;
+    [SerializeField] private Transform _sawEnd;
+    [SerializeField] private Transform _sawBody;
+    [SerializeField] private float _moveSpeed;
+    [SerializeField] private float _offset;
+
+    private float _positionT;
+
+    private void Awake()
+    {
+        _positionT = _offset;
+    }
+
+    private void Update()
+    {
+        _positionT += _moveSpeed * Time.deltaTime;
+        _sawBody.transform.position = Vector3.Lerp(_sawStart.position, _sawEnd.position, Mathf.PingPong(_positionT, 1));
+    }
+}
